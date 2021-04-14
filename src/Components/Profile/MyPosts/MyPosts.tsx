dce17b40744d301d462f52afbfabed2fe57a525a
Zak-11 <dianazakov@rambler.ru>
2021-04-15 00:04:51 +0300

    My posts.mep

--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -7,37 +7,37 @@ import {NavLink} from "react-router-dom";
 type MyPostsItemProps = {
     messages: string
     likesCount: number
-    id: number
+    //id: number
 }
-const MyPostsItem = (props:  MyPostsItemProps) => {
-    return<div className={classes.posts}>
-   <Post message={props.messages} likesCount={props.likesCount} ></Post>
+const MyPostsItem = (props: MyPostsItemProps) => {
+
+
+    return <div className={classes.posts}>
+        <Post  message={props.messages} likesCount={props.likesCount}></Post>
     </div>
 }
 
 export function MyPosts() {
-    let postData = [
-        {id: 1, message: 'Hi, how are you?', likesCount:2},
-        {id: 2, message: 'It,s my first post',  likesCount:26 },
-
-
+    let posts = [
+        {id: 1, message: 'Hi, how are you?', likesCount: 2},
+        {id: 2, message: 'It,s my first post', likesCount: 26},
     ]
+
+    let postsElements = posts.map(p => <MyPostsItem messages={p.message} likesCount={p.likesCount}/>)
 
     return (
         <div className={classes.postsBlock}>
-         <h3>My posts</h3>
+            <h3>My posts</h3>
             <div>
                 <div>
                     <textarea> </textarea>
                 </div>
                 <div>
-                <button>Add post</button>
+                    <button>Add post</button>
                 </div>
             </div>
             <div className={classes.posts}>
-                <Post message={postData[0].message} likesCount={postData [0].likesCount}/>
-                <Post message={postData[1].message} likesCount={postData [1].likesCount}/>
-
+                {postsElements}
             </div>
 
         </div>
